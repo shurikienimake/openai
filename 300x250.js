6ab837ab1af0fc779447e7ef6075bfa1abaa0913
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2023-02-22 12:47:09 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : 'c8f61acb585435b0d935785e6f7b658c',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/c8f61acb585435b0d935785e6f7b658c/invoke.js"></scr' + 'ipt>');
